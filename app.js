0bb8e0a6d3f88f30ef22d023d4f6ac7513bec76f
judinus10 <judes.20222403@iit.ac.lk>
2024-11-10 14:42:34 +0530

    constant added

---
+++ app.js
@@ -0,0 +1,5 @@
+const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe');
+const gradients = document.querySelectorAll('.gradient');
+const shoeBg = document.querySelector('.shoeBackground');
